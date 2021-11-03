10d24d18869d6432dc882679b0b757efb1c3037b
Xihai Luo <xihai.luo01@gmail.com>
2021-11-03 19:09:25 +0000

    created a connection object to server

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+//establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+  return conn;
+};
+
+console.log("Connecting ...");
+const conn = connect();
+conn.on('data', (data) => {
+  console.log(data);
+});
