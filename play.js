31c2a8fd5ec14c07e7c78e2d546477d22afa6048
Xihai Luo <xihai.luo01@gmail.com>
2021-11-03 20:26:43 +0000

    setup stdin event listener to handle key presses

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 //import functions
 const { connect } = require('./client');
+
+const handleUserInput = function(key) {
+  //terminate our program by checking for ctrl + c
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+//setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
