9f6c0f512dff79c76ccd4c5b9a7dad3b0e92d141
Xihai Luo <xihai.luo01@gmail.com>
2021-11-03 20:33:52 +0000

    refactored user input code; moved setUpInput and handleUserInput functions to a seperate file

---
+++ input.js
@@ -0,0 +1,19 @@
+const handleUserInput = function(key) {
+  //terminate our program by checking for ctrl + c
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+//setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+//export function
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 //import functions
 const { connect } = require('./client');
-
-const handleUserInput = function(key) {
-  //terminate our program by checking for ctrl + c
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-//setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
