e2df74bdb757e27372b09cdfa1dfbec046e6a9cb
Xihai Luo <xihai.luo01@gmail.com>
2021-11-04 01:08:45 +0000

    created a constants file and moved IP and PORT into the file

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 //establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   //interpret incoming data as text
   conn.setEncoding('utf8');
@@ -15,9 +16,6 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: UwU');
- /*    setInterval(() => {
-      conn.write('Move: up');
-    }, 500); */
   });
   return conn;
 };
---
+++ constants.js
@@ -0,0 +1,8 @@
+//constants
+const IP = '165.227.47.243';
+const PORT = 50541;
+
+module.exports = {
+  IP,
+  PORT
+};
